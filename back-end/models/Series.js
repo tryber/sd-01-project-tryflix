3368add0e13f8b452d6a3680dc1fe1e841b73e42
antoniopaolillo <antonio_francisco_p@live.com>
2020-06-09 18:51:09 -0300

    add endpoint details

--- back-end/models/Series.js
+++ back-end/models/Series.js
@@ -1,10 +1,17 @@
 const searchDataBase = require('./connection');
 
 class Series {
-  static async getAll () {
+  static async getAll() {
     const query = 'SELECT id, name, image, favorite FROM series ORDER BY name';
     const data = await searchDataBase(query);
     return data;
+  }
+
+  static async getSerieById(id) {
+    const query = `SELECT s1.id, s1.name, ge.type, s1.description, s1.release_date, s1.favorite, s1.image 
+    FROM series as s1 INNER JOIN genre as ge ON s1.genre_id = ge.id_genre WHERE s1.id = ${id}`;
+    const serie = await searchDataBase(query);
+    return serie[0];
   }
 }
 
